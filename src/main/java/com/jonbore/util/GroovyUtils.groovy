fe44c96120dc8d5b1debf003b08a13f9943ff183
bo.zhou1 <zhouboIT@163.com>
2017-12-08 12:43:05 +0800

    groovy 脚本测试依赖添加

--- src/main/java/com/jonbore/util/GroovyUtils.groovy
+++ src/main/java/com/jonbore/util/GroovyUtils.groovy
@@ -14,15 +14,15 @@ class GroovyUtils {
         def doc = parse.parse("e:/test1.xml")
 
         doc.attributes().each {
-            k,v ->
-                println k + ":" +v
+            k, v ->
+                println k + ":" + v
         }
         println '======================================'
         doc.movie.each {
             movie ->
-                movie.attributes().each{
-                    k,v ->
-                        println k + ":" +v
+                movie.attributes().each {
+                    k, v ->
+                        println k + ":" + v
                 }
         }
         println '======================================'
@@ -35,15 +35,15 @@ class GroovyUtils {
         }
     }
 
-    static void buildXml(){
-        def mp = [1 : ['Enemy Behind', 'War, Thriller','DVD','2003',
-                       'PG', '10','Talk about a US-Japan war'],
-                  2 : ['Transformers','Anime, Science Fiction','DVD','1989',
-                       'R', '8','A scientific fiction'],
-                  3 : ['Trigun','Anime, Action','DVD','1986',
-                       'PG', '10','Vash the Stam pede'],
-                  4 : ['Ishtar','Comedy','VHS','1987', 'PG',
-                       '2','Viewable boredom ']]
+    static void buildXml() {
+        def mp = [1: ['Enemy Behind', 'War, Thriller', 'DVD', '2003',
+                      'PG', '10', 'Talk about a US-Japan war'],
+                  2: ['Transformers', 'Anime, Science Fiction', 'DVD', '1989',
+                      'R', '8', 'A scientific fiction'],
+                  3: ['Trigun', 'Anime, Action', 'DVD', '1986',
+                      'PG', '10', 'Vash the Stam pede'],
+                  4: ['Ishtar', 'Comedy', 'VHS', '1987', 'PG',
+                      '2', 'Viewable boredom ']]
 
         def strWrite = new StringWriter()
         def mB = new MarkupBuilder(strWrite)
@@ -58,7 +58,7 @@ class GroovyUtils {
                         year(sd.value[3])
                         rating(sd.value[4])
                         stars(sd.value[4])
-                        description("value":"zhoubo",sd.value[5])
+                        description("value": "zhoubo", sd.value[5])
                     }
             }
         }
@@ -71,22 +71,22 @@ class GroovyUtils {
         fileWrite.flush()
         fileWrite.close()
     }
-    
+
     static void streamBuildXml() {
         def comment = "<![CDATA[<!-- address is new to this release -->]]>"
         def builder = new StreamingMarkupBuilder()
         builder.encoding = "UTF-8"
         def person = {
             mkp.xmlDeclaration()
-            mkp.pi("xml-stylesheet": "type='text/xsl' href='myfile.xslt'" )
-            mkp.declareNamespace('':'http://myDefaultNamespace')
-            mkp.declareNamespace('location':'http://someOtherNamespace')
-            person(id:100){
+            mkp.pi("xml-stylesheet": "type='text/xsl' href='myfile.xslt'")
+            mkp.declareNamespace('': 'http://myDefaultNamespace')
+            mkp.declareNamespace('location': 'http://someOtherNamespace')
+            person(id: 100) {
                 firstname("Jane")
                 lastname("Doe")
                 mkp.yieldUnescaped(comment)
                 location.address("123 Main")
-                }
+            }
         }
         def writer = new FileWriter("e:/person.xml")
         writer << builder.bind(person)
